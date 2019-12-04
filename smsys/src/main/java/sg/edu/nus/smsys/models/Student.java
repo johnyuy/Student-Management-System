fe5d0cf3b7ad9f42ec1c9e05f096197b97836a30
Natalie Hong <nataliehsm@gmail.com>
2019-12-04 17:13:13 +0800

    Working Creating Tables with Student

--- smsys/src/main/java/sg/edu/nus/smsys/models/Student.java
+++ smsys/src/main/java/sg/edu/nus/smsys/models/Student.java
@@ -8,6 +8,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
+
 @Entity
 @Table(name="student_table")
 public class Student extends Person{
