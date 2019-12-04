a5aad1719d23cf6cab64fc5f99d5cac4e93d5041
johnyuy <57468127+johnyuy@users.noreply.github.com>
2019-12-04 16:53:59 +0800

    Added Repository Classes for Student and Application

--- smsys/src/main/java/sg/edu/nus/smsys/models/Student.java
+++ smsys/src/main/java/sg/edu/nus/smsys/models/Student.java
@@ -7,7 +7,9 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
+import javax.persistence.Table;
 @Entity
+@Table(name="student_table")
 public class Student extends Person{
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
