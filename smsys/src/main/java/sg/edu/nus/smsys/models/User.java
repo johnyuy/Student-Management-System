9abc044df0e768aca1f67b310e652e2101969d59
Natalie Hong <nataliehsm@gmail.com>
2019-12-04 11:17:47 +0800

    Added User class for login

---
+++ smsys/src/main/java/sg/edu/nus/smsys/models/User.java
@@ -0,0 +1,62 @@
+package sg.edu.nus.smsys.models;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+
+@Entity
+public class User {
+	@Id
+	@NotEmpty
+	private String id;
+	@NotEmpty
+	private String password;
+	
+	
+	public User() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public User(String id, String password) {
+		super();
+		this.id = id;
+		this.password = password;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	
+	
+}
+
