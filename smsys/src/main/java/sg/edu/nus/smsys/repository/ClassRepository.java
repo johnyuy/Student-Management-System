cee068b04d26ffb12ac049b2989727395ed73a3d
JIANG YAZHI <JIANG YAZHI@172.23.213.248>
2019-12-04 18:39:22 +0800

    classRepository

--- smsys/src/main/java/sg/edu/nus/smsys/repository/ClassRepository.java
+++ smsys/src/main/java/sg/edu/nus/smsys/repository/ClassRepository.java
@@ -2,8 +2,7 @@ package sg.edu.nus.smsys.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-
-
+//save
 public interface ClassRepository extends JpaRepository<Class, Integer> {
-
+ 
 }
