1070a72da66687d114140a2a186f49ff108fbcc9
drakelin <drakelin@localhost>
2019-12-05 13:47:36 +0800

    Addition: 1. CourseRepository v1.0 2. CourseController v1.0

--- smsys/src/main/java/sg/edu/nus/smsys/repository/CourseClassRepository.java
+++ smsys/src/main/java/sg/edu/nus/smsys/repository/CourseClassRepository.java
@@ -2,9 +2,11 @@ package sg.edu.nus.smsys.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import sg.edu.nus.smsys.models.Course;
 import sg.edu.nus.smsys.models.CourseClass;
 
 
 public interface CourseClassRepository extends JpaRepository<CourseClass, Integer> {
+
  
 }
