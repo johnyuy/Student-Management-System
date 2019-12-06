26f0611ac8ad5cb99ab84889de470dce04a31124
juggernauntjo <johannfza@gmail.com>
2019-12-06 10:36:47 +0800

    Johann 6 Dec morning
    1)Changed application.properties to commit drop create tables
    
    2)Edited pom.xml to have 'bootstrap' dependencies
    
    3)Added Methods in Student Repository
    
    4)Created Student view, add, search View and controller

--- smsys/src/main/java/sg/edu/nus/smsys/repository/StudentRepository.java
+++ smsys/src/main/java/sg/edu/nus/smsys/repository/StudentRepository.java
@@ -1,9 +1,27 @@
 package sg.edu.nus.smsys.repository;
 
+import java.util.Collection;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 import sg.edu.nus.smsys.models.Student;
 
 public interface StudentRepository extends JpaRepository<Student, Integer> {
+
+	List<Student> findByFirstNameContaining(String firstName);
+	List<Student> findByMiddleNameContaining(String name);
+	List<Student> findByLastNameContaining(String name);
+	List<Student> findByEmailContaining(String name);
+	List<Student> findByStudentId(int id);
+
+	
+	@Query("select s from Student s where s.firstName like %?1% or s.middleName "
+			+ "like %?1% or s.lastName like %?1% or s.email like %?1%  ")
+	List<Student> findByStudentFullNameLike(String name);
+//	List<Student> findByStudentId(int id);
 	
 }
