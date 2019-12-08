fd8bbb518bb994484c47ffcfaa5a954b33def8d5
drakelin <drakelin@localhost>
2019-12-08 12:33:22 +0800

    Add Class Button v1.0

--- smsys/src/main/java/sg/edu/nus/smsys/controllers/CourseClassController.java
+++ smsys/src/main/java/sg/edu/nus/smsys/controllers/CourseClassController.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -46,5 +48,12 @@ public class CourseClassController {
 
 		
 		return("courseclassform");
+	}
+	
+	@PostMapping("/insert")
+	public String insertCourseClassController(@ModelAttribute CourseClass courseClass)
+	{
+		ccRepo.save(courseClass);
+		return "redirect:/courseClass/list";
 	}
 }
